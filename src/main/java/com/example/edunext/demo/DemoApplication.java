4a8b0508cff885cd4552518e23e0ed5d1e82066c
ravi <earic20pal@gmail.com>
2019-06-07 10:48:26 +0530

    commit by ravi for demo project spring boot

---
+++ src/main/java/com/example/edunext/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.edunext.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages={"com.example.edunext"})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Edu
+  Date: 07-06-2019
+  Time: 09:58
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+<h4>Test here
+</h4>
+
+</body>
+</html>
